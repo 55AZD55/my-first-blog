237099d13c9518a7c30afa0812a4a1a61964c06e
55AZD55 <azarenko.dmitrijj@yandex.ru>
2025-01-29 11:42:48 +0600

    Canged the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z38sk@ejc0ux7gmjwa&60g+!_+tk#=lti03^5)t=29s8cse1pt'
+SECRET_KEY = 'django-insecure-lwlu!-&wicka!*sbi5+n%o_vc+8ndkz(luwyu0n=5$+b6bi@gv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -115,7 +115,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
